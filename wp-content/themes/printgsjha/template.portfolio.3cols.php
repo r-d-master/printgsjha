6609ebea86922672164fac0c82ae01c8dc1c0343
Gauri Shankar Jha <gsjha@tcss.co.in>
2014-08-17 13:26:54 +0530

    add lightbox

--- wp-content/themes/printgsjha/template.portfolio.3cols.php
+++ wp-content/themes/printgsjha/template.portfolio.3cols.php
@@ -50,7 +50,7 @@ get_header(); ?>
 										<div class="details">
 											<?php the_title(); ?>
 										</div>
-										<!--<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" class="overlay fancybox"></a>-->
+										<a title="<?php the_title(); ?>" class="overlay"></a>
 									</li>
 									<?php
 									endwhile;?>
